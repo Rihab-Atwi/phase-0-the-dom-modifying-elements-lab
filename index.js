74d15246d83116c3868ec0f378ed3f566042c3de
Rihab_Atwi <rihab112000@gmail.com>
2022-08-27 20:09:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const mainVar = document.getElementById(`main`);
+mainVar.remove();
+
+const newHeader = document.createElement(`h1`);
+document.body.appendChild(newHeader);
+newHeader.id = `victory`;
+newHeader.innerHTML = `Anthony is the champion`;
